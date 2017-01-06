5ceff4a461d4df99cef1bcd476fbcc1a115b8827
PFXStudio <pfxstudio21@gmail.com>
2017-01-07 08:35:37 +0900

    Add PFXLockPincodeSDK Project

---
+++ PFXLockPincodeSDK/PFXLockPincodeSDK/PFXLockPincodeSDK.h
@@ -0,0 +1,19 @@
+//
+//  PFXLockPincodeSDK.h
+//  PFXLockPincodeSDK
+//
+//  Created by PFXStudio on 2017. 1. 7..
+//  Copyright © 2017년 PFXStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PFXLockPincodeSDK.
+FOUNDATION_EXPORT double PFXLockPincodeSDKVersionNumber;
+
+//! Project version string for PFXLockPincodeSDK.
+FOUNDATION_EXPORT const unsigned char PFXLockPincodeSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PFXLockPincodeSDK/PublicHeader.h>
+
+
